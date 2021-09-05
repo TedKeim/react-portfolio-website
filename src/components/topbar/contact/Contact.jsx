db51a1c25ff217efa2d2f88a3ae6d971c4ecc412
Ted Keim <tedkeim@yahoo.com>
2021-09-05 16:18:29 -0400

    made mobile responsive

--- src/components/topbar/contact/Contact.jsx
+++ src/components/topbar/contact/Contact.jsx
@@ -1,6 +1,14 @@
+import { useState } from 'react';
 import "./contact.scss"
 
 export default function Contact() {
+    const [message, setMessage] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMessage(true)
+    }
+    
     return (
         <div className='contact' id="contact">
             <div className="left">
@@ -8,10 +16,11 @@ export default function Contact() {
             </div>
             <div className="right">
                 <h2>Contact.</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input type="text" placeholder="Email"/>
                     <textarea placeholder="Message"></textarea>
                     <button type="submit">Send</button>
+                    {message && <span>Thanks, expect a reply ASAP!</span>}
                 </form>
             </div>
         </div>
